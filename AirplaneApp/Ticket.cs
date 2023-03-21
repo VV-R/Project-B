f55e95dc9386a067b7b2482ea1a7e879d0136938
CasGijtenbeek <cas@gijtenbeek.tk>
2023-03-21 15:32:17 +0100

    Added Flight class and changed Ticket class

--- AirplaneApp/Ticket.cs
+++ AirplaneApp/Ticket.cs
@@ -1,25 +1,13 @@
-public class Ticket
+public class Ticket : Flight
 {
-    public string FlightNumber;
     public string Name;
-    public int GateNumber;
     public string SeatNumber;
-    public string DepartureLocation;
-    public DateTime DepartureTime;
-    public string ArrivalLocation;
-    public DateTime ArrivalTime;
     public DateTime BoardingTime; // Departure time - x amount of time
 
-    public Ticket(string flightNumber, string name, int gateNumber, string seatNumber, string departureLocation, DateTime departureTime, string arrivalLocation, DateTime arrivalTime, DateTime boardingTime)
+    public Ticket(string flightNumber, string name, int gateNumber, string seatNumber, string departureLocation, DateTime departureTime, string arrivalLocation, DateTime arrivalTime, DateTime boardingTime) : base(flightNumber, gateNumber, departureLocation, departureTime, arrivalLocation, arrivalTime)
     {
-        FlightNumber = flightNumber;
         Name = name;
-        GateNumber = gateNumber;
         SeatNumber = seatNumber;
-        DepartureLocation = departureLocation;
-        DepartureTime = departureTime;
-        ArrivalLocation = arrivalLocation;
-        ArrivalTime = arrivalTime;
         BoardingTime = boardingTime;
     }
 
