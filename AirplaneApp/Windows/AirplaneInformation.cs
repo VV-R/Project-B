cae2fb6791beaf04db8b446a3aa9dc7c9e1132f4
Levi van Daalen <vandaalenlevi@gmail.com>
2023-03-29 13:24:18 +0200

    Added backbuttons to empty windows

--- AirplaneApp/Windows/AirplaneInformation.cs
+++ AirplaneApp/Windows/AirplaneInformation.cs
@@ -3,5 +3,14 @@ using Terminal.Gui;
 
 public class AirplaneInformation : Toplevel
 {
-    
+    public AirplaneInformation()
+    {
+        Button goBackButton = new Button() {
+            Text = "Terug"
+        };
+
+        goBackButton.Clicked += () => { WindowManager.GoBackOne(this);};
+
+        Add(goBackButton);
+    }
 }
--- AirplaneApp/Windows/Booking.cs
+++ AirplaneApp/Windows/Booking.cs
@@ -3,5 +3,14 @@ using Terminal.Gui;
 
 public class Booking : Toplevel
 {
-    
+    public Booking()
+    {
+        Button goBackButton = new Button() {
+            Text = "Terug"
+        };
+
+        goBackButton.Clicked += () => { WindowManager.GoBackOne(this);};
+
+        Add(goBackButton);
+    }
 }
