feea6948fa64a6c1af32e11c0c7483d1836b262a
janellacharju <1042022@hr.nl>
2023-04-17 09:47:34 +0200

    Buttons toegevoegd info vliegtuigen

--- AirplaneApp/Windows/AirplaneInformation.cs
+++ AirplaneApp/Windows/AirplaneInformation.cs
@@ -12,5 +12,20 @@ public class AirplaneInformation : Toplevel
         goBackButton.Clicked += () => { WindowManager.GoBackOne(this);};
 
         Add(goBackButton);
+
+        Button boeing737Button = new Button() {
+
+            Text = "Boeing 737"
+        };
+
+
+        Button airbusButton = new Button() {
+            Text = "Airbus 330-200"
+        };
+
+        Button boeing787Button = new Button() {
+            Text = "Boeing 787"
+        };
+        Add(boeing737Button,airbusButton,boeing787Button);
     }
 }
