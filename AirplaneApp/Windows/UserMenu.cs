61a5b18b724ce508d8718ed9f5f5cedcd529a3a3
Levi van Daalen <vandaalenlevi@gmail.com>
2023-04-19 12:25:01 +0200

    Fixed some bugs created by merges

--- AirplaneApp/Windows/UserMenu.cs
+++ AirplaneApp/Windows/UserMenu.cs
@@ -15,7 +15,7 @@ public class UserMenu : Toplevel
             Y = Pos.Bottom(nameLabel) + 1,
         };
 
-        infoButton.Clicked += () => { WindowManager.GoForwardOne(new EditUserInfo()); };
+        infoButton.Clicked += () => { WindowManager.GoForwardOne(new EditUserInfo(WindowManager.CurrentUser)); };
 
         Add(nameLabel, infoButton);
     }
