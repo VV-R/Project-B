f312f3fcf298824f2e8b9ea6dddc092b00266ead
Levi <vandaalenlevi@gmail.com>
2023-06-13 10:30:43 +0200

    fixed logmanager for windows

--- AirplaneApp/Managers/LogManager.cs
+++ AirplaneApp/Managers/LogManager.cs
@@ -4,7 +4,7 @@ public static class LogManager {
     public static string LogError(Exception e) => LogError(e.ToString());
     public static string LogError(string msg) {
         string logDir = "logs/";
-        string logFile = $"{logDir}{DateTime.Now.ToString("yyyy-MM-dd-HH:mm:ss")}.log";
+        string logFile = $"{logDir}{DateTime.Now.ToString("yyyy-MM-dd-HH=mm=ss")}.log";
         if (!Directory.Exists(logDir))
             Directory.CreateDirectory(logDir);
         using (var writer = new StreamWriter(logFile)) {
