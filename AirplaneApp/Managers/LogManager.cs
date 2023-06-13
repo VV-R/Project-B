b5f3213d68bb1d1ae31abe3b610c5f998090cd8d
Ruben van Voorden <rubenvanvoorden@me.com>
2023-06-13 19:18:34 +0200

    Add ability to logmanager to log individual strings to the same log file.

--- AirplaneApp/Managers/LogManager.cs
+++ AirplaneApp/Managers/LogManager.cs
@@ -1,14 +1,33 @@
 namespace Managers;
 
 public static class LogManager {
-    public static string LogError(Exception e) => LogError(e.ToString());
-    public static string LogError(string msg) {
+    private static readonly string _startTimeString;
+    private static readonly string _dateTimeFormat = "yyyy-MM-dd-HH=mm=ss";
+
+    static LogManager() {
+        _startTimeString = DateTime.Now.ToString(_dateTimeFormat);
+    }
+
+    private static StreamWriter _GetFile(string fileName, bool append = false) {
         string logDir = "logs/";
-        string logFile = $"{logDir}{DateTime.Now.ToString("yyyy-MM-dd-HH=mm=ss")}.log";
+        string logFile = $"{logDir}{fileName}";
         if (!Directory.Exists(logDir))
             Directory.CreateDirectory(logDir);
-        using (var writer = new StreamWriter(logFile)) {
-            writer.Write(msg);
+        return new StreamWriter(logFile, append);
+    }
+
+    public static string LogMessage(string msg) {
+        string logFile = _startTimeString + ".log";
+        using (StreamWriter writer = _GetFile(logFile, true)) {
+            writer.WriteLine(msg);
+        }
+        return logFile;
+    }
+
+    public static string LogError(Exception exc) {
+        string logFile = $"{DateTime.Now.ToString(_dateTimeFormat)}.log";
+        using (StreamWriter writer = _GetFile(logFile)) {
+            writer.Write(exc.ToString());
         }
         return logFile;
     }
